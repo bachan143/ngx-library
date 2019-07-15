5c4405404b3be22d21886897f153b2ec6bf6857b
Manoj Lenka <manojl@mindfiresolutions.com>
2019-07-15 11:26:22 +0530

    first commit

---
+++ src/cropperDrawSettings.d.ts
@@ -0,0 +1,10 @@
+export declare class CropperDrawSettings {
+    lineDash: boolean;
+    strokeWidth: number;
+    strokeColor: string;
+    cornerMarkerFillColor: string;
+    dragIconStrokeWidth: number;
+    dragIconStrokeColor: string;
+    dragIconFillColor: string;
+    constructor(settings?: any);
+}
